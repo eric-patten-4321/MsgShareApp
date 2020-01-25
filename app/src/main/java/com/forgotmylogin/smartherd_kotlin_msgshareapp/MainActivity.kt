24a47bda8a4a85aca884bbb1c4dbce913b31f845
eric-patten-4321 <eric.patten.4321@gmail.com>
2020-01-25 15:07:09 -0800

    Initial Commit

---
+++ app/src/main/java/com/forgotmylogin/smartherd_kotlin_msgshareapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.forgotmylogin.smartherd_kotlin_msgshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
