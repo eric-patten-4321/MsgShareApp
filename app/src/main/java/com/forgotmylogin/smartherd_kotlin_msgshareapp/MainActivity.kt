253525be2edbe1d8b909a472266083596bd71afb
eric-patten-4321 <eric.patten.4321@gmail.com>
2020-01-25 15:24:19 -0800

    Section two, video two

--- app/src/main/java/com/forgotmylogin/smartherd_kotlin_msgshareapp/MainActivity.kt
+++ app/src/main/java/com/forgotmylogin/smartherd_kotlin_msgshareapp/MainActivity.kt
@@ -2,12 +2,20 @@ package com.forgotmylogin.smartherd_kotlin_msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnShowToast.setOnClickListener {
+            Log.i("Main Activity", "Button was clicked")
+
+            Toast.makeText(this, "Button was clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
